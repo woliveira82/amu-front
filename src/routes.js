6419625e54bc0619ddefa00a234a9657e51a1ba8
Weiner <woliveira82@gmail.com>
2020-02-15 00:22:55 -0300

    Basic structure

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+
+import Email from "./pages/email";
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ Email } />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
