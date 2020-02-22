7df1a42bf94d76aabc72b71c31c5859d522db778
Weiner <woliveira82@gmail.com>
2020-02-22 20:11:15 -0300

    CSS correction and favicon

--- src/pages/email/index.js
+++ src/pages/email/index.js
@@ -6,21 +6,20 @@ import artigoDeMae from "./ArtigosdeMae.png";
 export default class Email extends Component {
     render () {
         return <div id="enterEmail">
-            <div class="box-content">
+            <div className="box-content box-image">
                 <img id="artigoDeMaeLogo" src={artigoDeMae} alt="Artigos de Mãe" />
             </div>
-            <div class="box-content bg-pink">
-                <p><h1>Nunca pare,</h1></p>
-                <p><h1>aprenda sempre</h1></p>
+            <div className="box-content box-text bg-pink">
+                <h1>Nunca pare,</h1>
+                <h1>aprenda sempre</h1>
             </div>
-            <div class="box-content">
-                <p>IMAGEM DE COMIDA</p>
+            <div className="box-content box-image" id="brigadeiros">
             </div>
-            <div class="box-content email-signup">
+            <div className="box-content box-text email-signup">
                 <h1>Oi confeiteira</h1>
                 <p>Estamos trabalhando muito para criar um curso abrangente que vai auxiliar você a crescer e ganhar prática na cozinha. Se quizer ficar por dentro das novidades e ganhar um desconto especial cadastre seu e-mail aqui.</p>
                 <div id="inputEmail">
-                    <input type="email"/><button>Cadastrar</button>
+                    <input type="email"/><button>CADASTRAR</button>
                 </div>
             </div>
         </div>;
